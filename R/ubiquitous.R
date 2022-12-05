3223f9fa5d634d24b27b8ac9cdabc831b7c59d74
pl-luk <lukas@smart-ies.de>
2022-12-05 19:58:42 +0100

    correct spelling error

--- R/ubiquitous.R
+++ R/ubiquitous.R
@@ -1,5 +1,5 @@
 
 equal_distribution_model <- function(alphabet, length) {
-  ps = rep(1 / length(alphabet), length(albphabet))
+  ps = rep(1 / length(alphabet), length(alphabet))
   sample(alphabet, length, replace = TRUE, prob = ps)
 }
