e40ed5eda4ccec888b4eda533fc026f43ba326c4
pl-luk <lukas@smart-ies.de>
2022-12-05 20:10:20 +0100

    add multinomial model and fix equal distribution

--- R/ubiquitous.R
+++ R/ubiquitous.R
@@ -1,5 +1,11 @@
 
-equal_distribution_model <- function(alphabet, length) {
+equal_distribution_model = function(alphabet, length) {
   ps = rep(1 / length(alphabet), length(alphabet))
-  sample(alphabet, length, replace = TRUE, prob = ps)
+  res = sample(alphabet, length, replace = TRUE, prob = ps)
+  return(res)
+}
+
+multinomial_distribution_model = function(alphabet, length, probabilities) {
+  res = sample(alphabet, length, replace = TRUE, prob = probabilities)
+  return(res)
 }
