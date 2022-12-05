b33e0e34641d33a25d8627bf712bf9a575d9a1a5
pl-luk <lukas@smart-ies.de>
2022-12-05 22:12:20 +0100

    Add markow model with according documentation

--- R/ubiquitous.R
+++ R/ubiquitous.R
@@ -26,3 +26,33 @@ multinomial_distribution_model = function(alphabet, length, probabilities) {
   res = sample(alphabet, length, replace = TRUE, prob = probabilities)
   return(res)
 }
+
+#' @title markov_model
+#'
+#' @description Generate a random sequence in the markow model with a specific lenght over a specified alphabet
+#' with a specified transition matrix and initial probabilities. Given an index set \eqn{I} with \eqn{r} elements then a \eqn{r\times r} matrix \eqn{(p_{ij})_{i,j \in I}}
+#' of nonnegative numbers is a transition matrix if \eqn{\forall i \in I: \sum_{j \in I} p_{ij} = 1}. \eqn{p_{ij}} is the probability that a state \eqn{i} is followed
+#' by state \eqn{j}. Given an Alphabet \eqn{\Sigma}, nonnegative numbers \eqn{(p_i)_{i \in \Sigma}} with sum \eqn{1} and a transition matrix \eqn{P = (p_{ij})_{i,j \in \Sigma}}
+#' the probability \eqn{\mathbb{P}(\textbf{s})} of a sequence \eqn{(\textbf{s})} over \eqn{\Sigma} with length \eqn{n} is calculated by
+#' \eqn{\mathbb{P}(\textbf{s}) = p_{s_1}\prod_{l = 2}^n p_{s_{n-1}s_n}}
+#' @param alphabet The alphabet the sequence should be based on as a vector
+#' @param length The length of the generated sequence as an integer
+#' @param matrix The transition matrix so that rows and columns are named by their respective letters which can be accomplished by \link{base}{colnames} and
+#' \link{base}{rownames}. \code{matrix[i, j]} describes the probability that a state \eqn{i} is followed by state \eqn{j}
+#' @param initial_probabilities A vector of probabilities that a sequence is starting with the \eqn{i}-th letter of the alphabet. If this argument is empty
+#' an equal distribution is assumed (which does not matter for large sequences)
+#' @return A vector that contains the letters of the generated sequence
+markow_model = function(alphabet, length, matrix, initial_probabilities = NULL ) {
+
+  if(is.null(initial_probabilities)) {
+    initial_probabilities = rep(1 / length(alphabet), length(alphabet))
+  }
+
+  res = character(length)
+  res[1] = sample(alphabet, 1, prob = initial_probabilities)
+  for(i in 2:length) {
+    res[i] = sample(alphabet, 1, prob = matrix[res[i - 1],])
+  }
+
+  return(res)
+}
