1717565d50a97b5c8285289fd7cc13b3aba48d26
pl-luk <lukas@smart-ies.de>
2022-12-05 19:42:47 +0100

    fix equal distribution function

--- R/ubiquitous.R
+++ R/ubiquitous.R
@@ -1,5 +1,5 @@
 
-equal_distribution_model(alphabet, length) {
+equal_distribution_model = function(alphabet, length) {
   ps = rep(1 / length(alphabet), length(albphabet))
   sample(alphabet, length, replace = TRUE, prob = ps)
 }
